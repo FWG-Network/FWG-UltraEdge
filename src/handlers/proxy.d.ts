e2b6b91d00d7a90405fb0cb6e1786b7bbb266a1e
FWG Network <sereyrathanak378@gmail.com>
2026-05-14 04:31:14 +0700

    Update proxy.d.ts

--- src/handlers/proxy.d.ts
+++ src/handlers/proxy.d.ts
@@ -1,2 +1,10 @@
-declare function FindProxyForURL(url: string, host: string): string;
-declare function shExpMatch(str: string, pattern: string): boolean;
+export {};
+
+declare global {
+  function FindProxyForURL(url: string, host: string): string;
+  function shExpMatch(str: string, pattern: string): boolean;
+  function dnsDomainIs(host: string, domain: string): boolean;
+  function dnsResolve(host: string): string;
+  function isInNet(host: string, pattern: string, mask: string): boolean;
+  function myIpAddress(): string;
+}
