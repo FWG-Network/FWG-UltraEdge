8caca407d8eb0eab0854e1d5e01e314f663b01e8
FWG Network <sereyrathanak378@gmail.com>
2026-05-13 07:49:00 +0000

    fix: exclude cinema-worker from ESLint scan

--- eslint.config.js
+++ eslint.config.js
@@ -3,9 +3,17 @@ import tsPlugin from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
 
 export default [
+  {
+    ignores: [
+      "cinema-worker/**",
+      "workers/**",
+      "dist/**",
+      "node_modules/**",
+    ]
+  },
   js.configs.recommended,
   {
-    files: ["**/*.ts"],
+    files: ["**/*.ts", "**/*.tsx"],
     languageOptions: { parser: tsParser },
     plugins: { "@typescript-eslint": tsPlugin },
     rules: { "no-undef": "off", "no-unused-vars": "off" }
