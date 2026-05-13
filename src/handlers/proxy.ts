e024e3a55e01f9a1d7633547c4a18036c6894802
FWG Network <sereyrathanak378@gmail.com>
2026-05-14 00:23:41 +0700

    Update proxy.ts

--- src/handlers/proxy.ts
+++ src/handlers/proxy.ts
@@ -4,7 +4,7 @@ declare function isInNet(ip: string, net: string, mask: string): boolean;
 declare function myIpAddress(): string;
 
 const PAC_SCRIPT = `function FindProxyForURL(url, host) {
-  var proxy = "PROXY proxy.ultraedge-prod.fasterwgseverkh.workers.dev/:443";
+  var proxy = "PROXY proxy.fasterwgserverkh.cloudflareaccess.com/:443";
 
   if (
     // --- បន្ថែមសម្រាប់ល្បឿនវីដេអូ (YouTube & Google Video) ---
