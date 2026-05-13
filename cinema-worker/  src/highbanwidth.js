dd59ba011a813e440aedfb9f934a136e07d11a8a
FWG Network <sereyrathanak378@gmail.com>
2026-05-14 04:06:07 +0700

    Update and rename proxy.js to highbanwidth.js

--- cinema-worker/  src/highbanwidth.js
+++ cinema-worker/  src/highbanwidth.js
@@ -1,6 +1,6 @@
 // @ts-nocheck
-declare function shExpMatch(str: string, shexp: string): boolean;
-    // 🟢 1. LOCAL NETWORK (រត់ត្រង់ក្នុងស្រុក គ្មាន Delay)
+// src/handlers/proxy.d.ts
+export function handleProxy(...args: any[]): any;    // 🟢 1. LOCAL NETWORK (រត់ត្រង់ក្នុងស្រុក គ្មាន Delay)
     if (isPlainHostName(host) ||
         isInNet(dnsResolve(host), "127.0.0.1", "255.0.0.0") ||
         isInNet(dnsResolve(host), "192.168.0.0", "255.255.0.0") ||
