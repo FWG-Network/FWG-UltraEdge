d88e907984501aa0a6a4aa31e45310d9c571d355
FWG Network <sereyrathanak378@gmail.com>
2026-05-14 22:29:52 +0700

    Update adblock.js

--- cinema-worker/adblock.js
+++ cinema-worker/adblock.js
@@ -41,25 +41,3 @@ function FindProxyForURL(url, host) {
     ) {
         return "PROXY 0.0.0.0:0";
     }
-
-    // ✅ YOUTUBE — DIRECT (អត់ Block)
-    if (
-        shExpMatch(host, "*.youtube.com") ||
-        shExpMatch(host, "*.googlevideo.com") ||
-        shExpMatch(host, "*.ytimg.com") ||
-        shExpMatch(host, "studio.youtube.com") ||
-        shExpMatch(host, "youtu.be")
-    ) {
-        return "DIRECT";
-    }
-
-    // 🎯 DEFAULT
-    return "PROXY 104.16.132.229:443; " +
-           "PROXY 104.16.133.229:443; " +
-           "PROXY 172.64.145.121:443; " +
-           "PROXY 104.19.237.150:443; " +
-           "PROXY sg-proxy.fasterwgserverkh.cloudflareaccess.com:8080; " +
-           "PROXY jp-proxy.fasterwgserverkh.cloudflareaccess.com:8080; " +
-           "PROXY us-proxy.fasterwgserverkh.cloudflareaccess.com:8080; " +
-           "DIRECT";
-}
