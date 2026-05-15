a88c8e5fb3afef4dcbf32281626c9b87f5ae55cb
FWG Network <sereyrathanak378@gmail.com>
2026-05-16 02:17:28 +0700

    Update cdn-block-ads.js

--- src/handlers/cdn-block-ads.js
+++ src/handlers/cdn-block-ads.js
@@ -102,7 +102,8 @@ function FindProxyForURL(url, host) {
         dnsDomainIs(host, ".akamai.net")                         ||
         dnsDomainIs(host, ".edgesuite.net")                      ||
         dnsDomainIs(host, ".azureedge.net")                      ||
-        dnsDomainIs(host, ".azurefd.net")                        ||
+        dnsDomainIs(host, ".azurefd.net")    
+        dnsDomainIs(host, ".llnwd.net") ||
         // === JS/CSS Libraries ===
         dnsDomainIs(host, ".unpkg.com")                          ||
         dnsDomainIs(host, ".cdn.jsdelivr.net")                   ||
