29758cd89ef6dda39e072ee4a9087cef7d82da60
FWG Network <sereyrathanak378@gmail.com>
2026-05-19 01:37:58 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-// ១. Import កូដដែលបងទើបតែបង្កើតមិញហ្នឹង
-import SecurityAudit from './components/SecurityAudit';
+import SecurityAudit from './SecurityAudit';
 
 function App() {
   return (
     <div className="App">
-      {/* ២. ដាក់ Tag ឈ្មោះកូដហ្នឹង ដើម្បីឱ្យវាបង្ហាញខ្លួន */}
       <SecurityAudit />
     </div>
   );
