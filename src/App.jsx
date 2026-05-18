44d85d66b9c0cf215bde908f71f891a6973c70d9
FWG Network <sereyrathanak378@gmail.com>
2026-05-19 01:10:49 +0700

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// ១. Import កូដដែលបងទើបតែបង្កើតមិញហ្នឹង
+import SecurityAudit from './components/SecurityAudit';
+
+function App() {
+  return (
+    <div className="App">
+      {/* ២. ដាក់ Tag ឈ្មោះកូដហ្នឹង ដើម្បីឱ្យវាបង្ហាញខ្លួន */}
+      <SecurityAudit />
+    </div>
+  );
+}
+
+export default App;
