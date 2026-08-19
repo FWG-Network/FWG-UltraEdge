63ab917488dcde131725d787d009a7c5eb549809
FWG Network <sereyrathanak378@gmail.com>
2026-08-20 02:48:20 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
-// =========================================================
+
 // FWG Live Stream Edge Worker — Zero-Lag 1080p+
 // Optimized for HLS / DASH live & VOD over Cloudflare
-// ===========================================================
-
 export interface Env {
   BACKEND_URL: string;
   FWG_API_SECRET: string;
